a35f3f7ccfe45f68f1fa0a670a422f8fa4d5f2cc
trungbui3044123 <trungbui304@gmail.com>
2025-06-28 09:28:46 +0900

    xu ly bat dong bo

--- Bai tap lon/logic.js
+++ Bai tap lon/logic.js
@@ -4,10 +4,11 @@ class LogicUI {
     this.data = thread.renderTable() || [];
     this.init();
   }
-  init() {
+  async init() {
     this.renderPaginationBTN();
     this.renderHtml(0);
     this.searchData();
+    this.addForm();
 
   }
   renderTbody(record) {
@@ -22,8 +23,8 @@ class LogicUI {
             <td>${record.title}</td>
             <td>${tagCells}</td>
             <td>${record.views}</td>
-            <td><button class="EditBtn">Edit</button></td>
-            <td><button class="DeleteBtn">Delete</button></td>    
+            <td><button class="EditBtn" onclick="ui.test()">Edit</button></td>
+            <td><button class="DeleteBtn" onclick="ui.deleteData(event)">Delete</button></td>    
         `;
     this.Tbody.appendChild(tr);
   }
@@ -75,38 +76,57 @@ class LogicUI {
     });
   }
 
-
-
-}
-const ui = new LogicUI();
-function resetaddForm(){
-
-}
-function    addData(){
+  addForm() {
     const toAddForm = document.getElementById("toAddForm");
-    toAddForm.addEventListener("click",(e)=>{
-       const addPostForm = document.getElementById("addForm");
-       addPostForm.style.display="block"
+    toAddForm.addEventListener("click", (e) => {
+      const addPostForm = document.getElementById("addForm");
+      addPostForm.style.display = "block";
 
-         submitData();
-    })
+     return this.submitData();
+    });
   }
-  function submitData(){
-    const addForm = document.getElementById("addForm");
-    addForm.addEventListener("submit",(e)=>{
-        e.preventDefault()
-           const title = document.getElementById("title");
-    const tags = document.getElementById("tags");
-    const views = document.getElementById("views");
-    const newObject = new PostsObject(0,title.value,"",tags.value,"",views.value);
-        confirm(`You have added this ${newObject.title} successfully `)
-        ui.data=thread.createData(newObject);
-        ui.data
-       console.log(ui.data)
-        const addPostForm = document.getElementById("addForm");
-        addPostForm.style.display="none"
-    })
-  }
-addData()
+
+   submitData() {
+  const addForm = document.getElementById("addForm");
+  addForm.addEventListener("submit",async (e) => {
+     
+    const title =await document.getElementById("title");
+    const tags =await document.getElementById("tags");
+    const views =await document.getElementById("views");
+    const newObject = new PostsObject(
+      0,
+      title.value,
+      "",
+      tags.value,
+      "",
+      views.value
+    );
+    // confirm(`You have added this ${newObject.title} successfully `);
+    this.data = thread.createData(newObject);
+    const addPostForm = document.getElementById("addForm");
+    addPostForm.style.display = "none";
+  });
+}
+//
+ deleteData(e){
+    const targetDOM = e.target;
+    if(targetDOM.textContent!=="Delete"){return}
+    const deleteID= targetDOM.closest("tr").dataset
+    const targetRow = targetDOM.closest("tr")
+    targetRow.remove()
+      this.data = thread.deleteData(deleteID);
+    this.renderHtml(0);
+    console.log(targetRow)
+ }
+
+}
+
+const ui = new LogicUI();
+
+// với btn động thì add luôn cái instance. function() vào btn đó khi sinh ra. Như vậy thì ko bị bắt bất dộng bộ
+//  Với 2 function trong cùng 1 class mà cái chạy trước, trả ra kết quả của cái chạy sau
+// tại function chạy sau áp dụng synce và await ở đúng value cần đợi
+// tại function chạy trước, thì phải return cái function chạy sau khi gọi ở đó.
+
 // document.querySelector("EditBtn").addEventListener("click",(e)=>this.editFunction());
-// document.querySelector("DeleteBtn").addEventListener("click",(e)=>this.deleteFunction());
+//
